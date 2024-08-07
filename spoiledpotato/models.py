d8270599c69f792c7b72f7dfc94a8e31ed06d09f
tflinch <tflinch16@gmail.com>
2024-08-07 10:18:39 -0400

    feat: add Movie  model

--- spoiledpotato/models.py
+++ spoiledpotato/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -10,3 +11,13 @@ class Actor(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Movie(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE, related_name='movies')
+    title = models.CharField(max_length=100)
+    length = models.DurationField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    preview_url= models.TextField()
+
+    def __str__(self):
+        return self.title 
