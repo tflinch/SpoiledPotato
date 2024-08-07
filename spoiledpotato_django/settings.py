81e4a9bdb06dbd197ff1e46d043167266ca2698a
tflinch <tflinch16@gmail.com>
2024-08-07 00:42:33 -0400

    feat: configure superuser

--- spoiledpotato_django/settings.py
+++ spoiledpotato_django/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'spoiledpotato_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spoiledpotato',
+        'USER': 'spoileduser',
+        'PASSWORD': 'potato',
+        'HOST': 'localhost'
     }
 }
 
