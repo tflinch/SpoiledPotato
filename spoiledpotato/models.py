562aeb03209414a0588acb7218592c2ef5bc3bbd
tflinch <tflinch16@gmail.com>
2024-08-07 01:23:04 -0400

    feat: add Actor model

--- spoiledpotato/models.py
+++ spoiledpotato/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    birthday = models.CharField(max_length=100)
+    about = models.TextField()
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
