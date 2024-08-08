2aa9bb3b9f5e02a457d370356ea46e423475b2fc
tflinch <tflinch16@gmail.com>
2024-08-08 01:10:39 -0400

    feat: add actor_detail

--- spoiledpotato/urls.py
+++ spoiledpotato/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
     path('actors/', views.actor_list, name='actor_list'),
+    path('actor/<int:pk>', views.actor_detail, name='actor_detail'),
     path('movie/<int:pk>', views.movie_detail, name='movie_detail')
 ]
--- spoiledpotato/views.py
+++ spoiledpotato/views.py
@@ -5,6 +5,10 @@ def actor_list(request):
     actors = Actor.objects.all()
     return render(request, 'spoiledpotato/actor_list.html',{'actors':actors})
 
+def actor_detail(request, pk):
+    actor = Actor.objects.get(id=pk)
+    return render(request, 'spoiledpotato/actor_detail.html', {'actor': actor})
+
 def movie_list(request):
     movies = Movie.objects.all()
     return render(request, 'spoiledpotato/movie_list.html', {'movies': movies})
