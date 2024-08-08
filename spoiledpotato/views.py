9783f3a6bfcdd3910b62ab312bd7994641cecabe
tflinch <tflinch16@gmail.com>
2024-08-07 22:00:14 -0400

    feat: add actor_list, movie_list

--- spoiledpotato/views.py
+++ spoiledpotato/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Movie, Actor
+
+def actor_list(request):
+    actors = Actor.objects.all()
+    return render(request, 'spoiledpotato/actor_list.html',{'actors':actors})
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'spoiledpotato/movie_list.html', {'movies': movies})
 
 # Create your views here.
