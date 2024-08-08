227debb866ef71ffd5068fb1d640d0b52d27959f
tflinch <tflinch16@gmail.com>
2024-08-08 13:54:59 -0400

    feat: django-select2 multi search select feature for form

--- spoiledpotato/forms.py
+++ spoiledpotato/forms.py
@@ -2,9 +2,17 @@ from django import forms
 from .models import Movie, Actor
 
 
+from django_select2.forms import Select2MultipleWidget
+
 class MovieForm(forms.ModelForm):
+    actors = forms.ModelMultipleChoiceField(
+        queryset=Actor.objects.all(),
+        widget=Select2MultipleWidget,
+        required=False
+    )
 
     class Meta:
         model = Movie
-        fields = ('title','actors' ,'length', 'rating', 'preview_url')
+        fields = ('title', 'actors', 'length', 'rating', 'preview_url')
+
 
--- spoiledpotato_django/settings.py
+++ spoiledpotato_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'spoiledpotato',
+    'django_select2',
 ]
 
 MIDDLEWARE = [
