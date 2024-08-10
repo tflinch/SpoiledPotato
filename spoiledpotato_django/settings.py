6199d73a228ffda43a8ab5e2eac1f0051a093b72
tflinch <tflinch16@gmail.com>
2024-08-10 16:43:15 -0400

    feat: gitignore integration

--- spoiledpotato_django/settings.py
+++ spoiledpotato_django/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +82,10 @@ WSGI_APPLICATION = 'spoiledpotato_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spoiledpotato',
-        'USER': 'spoileduser',
-        'PASSWORD': 'potato',
-        'HOST': 'localhost'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
     }
 }
 
