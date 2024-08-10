e9bd067ce6027ebb3921f25febc7e5c080bd5449
tflinch <tflinch16@gmail.com>
2024-08-10 17:56:35 -0400

    update

--- spoiledpotato_django/settings.py
+++ spoiledpotato_django/settings.py
@@ -28,7 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-nxurl5fn^0e*wud4t2)=d3%k-@=p0c%ne_n$g*tjiu6$&stdj6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
